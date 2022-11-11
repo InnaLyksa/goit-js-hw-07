f80a86aba0c723905a5d15d096f6359cf4b5c25f
InnaLyksa <in.lyksa@gmail.com>
2022-11-11 10:01:17 +0200

    Task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,57 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryItemsMarkup = createGalleryItems(galleryItems);
+
+function createGalleryItems(items) {
+  return items
+    .map(item => {
+      return `
+      <div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+galleryContainer.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
+galleryContainer.addEventListener('click', onImageClick);
+
+function onImageClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const modal = basicLightbox.create(
+    `
+     <img src="${event.target.dataset.source}">
+`,
+    {
+      onShow: modal => {
+        document.addEventListener('keydown', onPressEscape);
+      },
+
+      onClose: modal => {
+        document.removeEventListener('keydown', onPressEscape);
+      },
+    },
+  );
+
+  modal.show();
+
+  function onPressEscape(event) {
+    if (event.code === 'Escape') {
+      modal.close();
+    }
+  }
+}
