9eed3464f5b1879ecf31d9404f9c2bb45756d173
InnaLyksa <in.lyksa@gmail.com>
2022-11-11 10:33:56 +0200

    Task-2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryItemsMarkup = createGalleryItems(galleryItems);
+
+function createGalleryItems(items) {
+  return items
+    .map(item => {
+      return `
+  <a class="gallery__item" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+  </a>
+`;
+    })
+    .join('');
+}
+galleryContainer.insertAdjacentHTML('afterbegin', galleryItemsMarkup);
+
+const lightbox = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
